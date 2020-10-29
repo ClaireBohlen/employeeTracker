3b1400aac1ba1952b9fa2b2b51903e6d16f17c9a
Claire Bohlen <clairebohlen@gmail.com>
2020-10-29 16:28:11 -0600

    fixing bugs in table and test in work bench

--- schema.sql
+++ schema.sql
@@ -5,29 +5,34 @@ CREATE database employee_DB;
 USE employee_DB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, --   PRIMARY KEY (position)
-    name VARCHAR(30) --   * **name** - VARCHAR(30) to hold department name
-    PRIMARY KEY (id) --   * **id** - INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30),
+    PRIMARY KEY (id) 
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT, --   * **id** - INT PRIMARY KEY
-    title VARCHAR (30), --   * **title** -  VARCHAR(30) to hold role title
-    salary DECIMAL(10,4) NULL, --   * **salary** -  DECIMAL to hold role salary
-    department_id INT --   * **department_id** -  INT to hold reference to department role belongs to
---!!!!! FIGURE OUT HOW TO CONNECT THIS ID WITH PRIMARY KEY OF DEPARTMENT TABLE
-
+    id INT NOT NULL AUTO_INCREMENT, 
+    title VARCHAR (30), 
+    salary DECIMAL(10,4) NULL, 
+    department_id INT,
+    PRIMARY KEY (id)
 
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT, --   * **id** - INT PRIMARY KEY
-    first_name VARCHAR(30),   --   * **first_name** - VARCHAR(30) to hold employee first name
-    last_name VARCHAR(30),   --   * **last_name** - VARCHAR(30) to hold employee last name
-    role_id INT,             --!!!! CONNECT TO ROLES   * **role_id** - INT to hold reference to role employee has
-    manager_id INT --   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+    id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(30),   
+    last_name VARCHAR(30),  
+    role_id INT, 
+    manager_id INT,
+    PRIMARY KEY (id)
+    
+    
+   
 
 );
+
+
 
 
   
